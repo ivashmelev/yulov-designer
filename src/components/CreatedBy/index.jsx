a8b62e55e8ebbe8aad5f2ac25455139523786c24
ivashmelev <shmelevivan20@gmail.com>
2020-05-19 16:04:03 +0300

    adaptive tablet

--- src/components/CreatedBy/index.jsx
+++ src/components/CreatedBy/index.jsx
@@ -1,15 +1,15 @@
-import React, {memo} from 'react';
-import {Link} from "react-router-dom";
+import React, { memo } from 'react';
+import { Link } from "react-router-dom";
 import styles from './CreatedBy.module.scss';
 
 const CreatedBy = memo(() => {
 
     return (
         <div className={styles.wrapper}>
-            <span>Made by&nbsp;</span>
-            <Link to={''}>Aleksey Y.</Link>
-            <span>&nbsp;&&nbsp;</span>
-            <a href={'tg://resolve?domain=ishmelev'}>FoxStudio</a>
+            <span className={styles.text}>Made by&nbsp;</span>
+            <Link className={styles.link} to={''}>Aleksey Y.</Link>
+            <span className={styles.text}>&nbsp;&&nbsp;</span>
+            <a className={styles.link} href={'tg://resolve?domain=ishmelev'}>FoxStudio</a>
         </div>
     )
 });
--- src/components/Element/index.jsx
+++ src/components/Element/index.jsx
@@ -18,39 +18,39 @@ const Element = memo(({ title, subtitle, index, img, description, right, link })
     const unhoverLink = () => setIsHoveredLink(false);
 
     return (
-        <div className={ styles.wrapper }>
-            <div className={ `${styles.header} ${right && styles.right}` }>
-                <span className={ styles.title }>
-                    { title }&nbsp;
-                </span>
-                <span className={ styles.subtitle }>
-                    - { subtitle }
-                </span>
-            </div>
-            <div className={ `${styles.content_wrapper} ${right && styles.right}` }>
-                <div className={ `${styles.content} ${right && styles.right}` }>
-                    <div className={ `${styles.number} ${right && styles.right}` }>0{ index + 1 }</div>
-                    <div className={ `reflection-container ${isHoveredLink && 'active'}` } onMouseEnter={ hoverImg } onMouseLeave={ unhoverImg }>
-                        { fragments.map((fragment, index) => {
-                            return <div key={ index } className={ `reflection-grid-cell reflection-grid-cell-${index}` } />
+        <div className={styles.wrapper}>
+            <div className={`${styles.content_wrapper} ${right && styles.right}`}>
+                <div className={`${styles.content} ${right && styles.right}`}>
+                    <div className={`${styles.number} ${right && styles.right}`}>0{index + 1}</div>
+                    <div className={`reflection-container ${isHoveredLink && 'active'}`} onMouseEnter={hoverImg} onMouseLeave={unhoverImg}>
+                        {fragments.map((fragment, index) => {
+                            return <div key={index} className={`reflection-grid-cell reflection-grid-cell-${index}`} />
                         }
-                        ) }
-                        <div className='reflection-content' style={ { backgroundImage: `url(${img})` } } />
+                        )}
+                        <div className='reflection-content' style={{ backgroundImage: `url(${img})` }} />
                     </div>
-                    <div className={ styles.parent_description_wrapper }>
-                        <div className={ `${styles.description_wrapper} ${right && styles.right}` }>
-                            <span className={ styles.description }>
-                                { description }
+                    <div className={styles.parent_description_wrapper}>
+                        <div className={`${styles.header} ${right && styles.right}`}>
+                            <span className={styles.title}>
+                                {title}&nbsp;
                             </span>
-                            <div className={ styles.link_wrapper }>
-                                <Link className={ `${styles.link} ${isHoveredImg && styles.active}` } to={ link }
-                                    onMouseEnter={ hoverLink } onMouseLeave={ unhoverLink }
+                            <span className={styles.subtitle}>
+                                - {subtitle}
+                            </span>
+                        </div>
+                        <div className={`${styles.description_wrapper} ${right && styles.right}`}>
+                            <span className={styles.description}>
+                                {description}
+                            </span>
+                            <div className={styles.link_wrapper}>
+                                <Link className={`${styles.link} ${isHoveredImg && styles.active}`} to={link}
+                                    onMouseEnter={hoverLink} onMouseLeave={unhoverLink}
                                 >
                                     Посмотреть проект >>
                                 </Link>
                             </div>
-                            <div className={ styles.soc_wrapper }>
-                                <Networks config={ networksConfig } />
+                            <div className={styles.soc_wrapper}>
+                                <Networks config={networksConfig} />
                             </div>
                         </div>
                     </div>
