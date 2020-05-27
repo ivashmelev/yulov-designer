fbf9bdf7ff8978ed0ac8747b109d549ae4dd468d
Ivan Shmelev <shmelevivan20@gmail.com>
2020-05-27 22:30:22 +0300

    fix

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { memo, useState } from 'react';
 import styles from './Navigation.module.scss';
 
@@ -5,27 +7,30 @@ const Navigation = memo(() => {
 
     const [node, setNode] = useState('about');
 
-    const scroll = (e, target) => {
+    const scroll = (target) => {
         setNode(target);
         document.querySelector(`#${target}`).scrollIntoView({ behavior: 'smooth' });
-    }
+    };
 
     return (
-        <div className={ styles.wrapper }>
-            <div className={ styles.block_wrapper }>
-                <div className={ styles.block }>
-                    <a className={ `${styles.link} ${node === 'about' && styles.active}` }
-                        onClick={ () => scroll('about') }
+        <div className={styles.wrapper}>
+            <div className={styles.block_wrapper}>
+                <div className={styles.block}>
+                    <a
+                      className={`${styles.link} ${node === 'about' && styles.active}`}
+                      onClick={() => scroll('about')}
                     >
                         обо мне
                     </a>
-                    <a className={ `${styles.link} ${node === 'portfolio' && styles.active}` }
-                        onClick={ () => scroll('portfolio') }
+                    <a
+                      className={`${styles.link} ${node === 'portfolio' && styles.active}`}
+                      onClick={() => scroll('portfolio')}
                     >
                         портфолио
                     </a>
-                    <a className={ `${styles.link} ${node === 'contacts' && styles.active}` }
-                        onClick={ () => scroll('contacts') }
+                    <a
+                      className={`${styles.link} ${node === 'contacts' && styles.active}`}
+                      onClick={() => scroll('contacts')}
                     >
                         контакты
                     </a>
@@ -33,7 +38,7 @@ const Navigation = memo(() => {
 
             </div>
         </div>
-    )
+    );
 });
 
 export default Navigation;
